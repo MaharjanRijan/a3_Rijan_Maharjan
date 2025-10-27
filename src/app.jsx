6d609c65bd1fc57a106712d6d72037abfec8b7be
Rijan Maharjan <maharijan127@gmail.com>
2025-10-27 17:11:10 -0400

    Added basic React App component with welcome message and implemented css styling

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Card Game</h1>
+            <p>Welcome to the card game!</p>
+        </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
